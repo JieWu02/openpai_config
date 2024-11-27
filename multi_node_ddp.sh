d2515e744ea7f87e1b5ecf520db5a1107a94d976
Wu Jie <83527625+2592761383@users.noreply.github.com>
2024-11-27 17:35:39 +0800

    Update multi_node_ddp.sh

--- multi_node_ddp.sh
+++ multi_node_ddp.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-# 设置NCCL参数
 export NCCL_BLOCKING_WAIT=1
 export NCCL_ASYNC_ERROR_HANDLING=1
-export NCCL_TIMEOUT=100
+export NCCL_TIMEOUT=1000
 
-# 使用swift内置的分布式训练支持
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
 NNODES=2 \
 NODE_RANK=$NODE_RANK \
@@ -13,6 +11,7 @@ MASTER_PORT=$MASTER_PORT \
 NPROC_PER_NODE=8 \
 swift pt \
     --model_type qwen2_5-7b \
+    --model_id_or_path /our_data/code_data/data/model/Qwen2.5-7B \
     --dataset /our_data/code_data/data/filter_data.json \
     --num_train_epochs 1 \
     --sft_type full \
@@ -27,7 +26,7 @@ swift pt \
     --weight_decay 0.1 \
     --learning_rate 3e-5 \
     --gradient_accumulation_steps 16 \
-    --train_dataset_sample 30000 \
+    --train_dataset_sample 100000 \
     --max_grad_norm 1.0 \
     --warmup_ratio 0.03 \
     --eval_steps 2000 \
