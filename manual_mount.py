592141bbb1d9253f3804e2095188f2faf6f85e17
Wu Jie <83527625+2592761383@users.noreply.github.com>
2024-11-27 11:24:45 +0800

    Update manual_mount.py

--- manual_mount.py
+++ manual_mount.py
@@ -9,14 +9,14 @@ os.system("ls -l /mnt")
 
 # Mount blob1
 # Do not use "/mnt/output" as mount point
-mkdir_and_chown("/mnt/haoling")
+mkdir_and_chown("/mnt/jiewu")
 
-mkdir_and_chown("/mnt/azcache/haoling")
+mkdir_and_chown("/mnt/azcache/jiewu")
 
-os.system("blobfuse2 mount /mnt/haoling --config-file=haoling_config.yaml")
+os.system("blobfuse2 mount /mnt/jiewu --config-file=jiewu_config.yaml")
 
 
 # If needed mount blob2
 # ......
 
-os.system("ls -l /mnt/haoling")
+os.system("ls -l /mnt/jiewu")
