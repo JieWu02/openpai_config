ffa3b5ef9ffc402e2302b1f11f81470610aa466d
Wu Jie <83527625+2592761383@users.noreply.github.com>
2025-01-28 17:50:01 +0800

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -5,10 +5,10 @@ export NODE_RANK=${PAI_CURRENT_TASK_ROLE_CURRENT_TASK_INDEX:=0}
 split_num=32
 node_count=${PAI_TASK_ROLE_TASK_COUNT_worker} # 64
 node_rank=$NODE_RANK
-blobkey="?sv=2023-01-03&st=2024-11-27T03%3A02%3A36Z&se=2024-12-03T03%3A02%3A00Z&skoid=c275b492-fed2-47ef-8704-1bb10e68fcf2&sktid=72f988bf-86f1-41af-91ab-2d7cd011db47&skt=2024-11-27T03%3A02%3A36Z&ske=2024-12-03T03%3A02%3A00Z&sks=b&skv=2023-01-03&sr=c&sp=racwdxltf&sig=lNZS14iL13QSIUCLw1Aro%2FNqVZmNBnUg072a2L6AXT4%3D"
+blobkey="?sv=2023-01-03&st=2025-01-28T07%3A19%3A58Z&se=2025-02-04T07%3A19%3A00Z&skoid=c275b492-fed2-47ef-8704-1bb10e68fcf2&sktid=72f988bf-86f1-41af-91ab-2d7cd011db47&skt=2025-01-28T07%3A19%3A58Z&ske=2025-02-04T07%3A19%3A00Z&sks=b&skv=2023-01-03&sr=c&sp=racwdxltf&sig=%2FfWj2c6V8wjpMDVK96AHvzYqnNfibfzwLmvMbnfAM4I%3D"
 
-./azcopy copy --recursive "https://xinzhanginterns.blob.core.windows.net/jiewu/data/sft_data?sv=2023-01-03&st=2024-11-27T03%3A02%3A36Z&se=2024-12-03T03%3A02%3A00Z&skoid=c275b492-fed2-47ef-8704-1bb10e68fcf2&sktid=72f988bf-86f1-41af-91ab-2d7cd011db47&skt=2024-11-27T03%3A02%3A36Z&ske=2024-12-03T03%3A02%3A00Z&sks=b&skv=2023-01-03&sr=c&sp=racwdxltf&sig=lNZS14iL13QSIUCLw1Aro%2FNqVZmNBnUg072a2L6AXT4%3D" "/our_data/code_data/data"
-./azcopy copy --recursive "https://xinzhanginterns.blob.core.windows.net/jiewu/data/output/test/qwen2_5-7b/v0-20241127-184146/checkpoint-7147?sv=2023-01-03&st=2024-11-27T03%3A02%3A36Z&se=2024-12-03T03%3A02%3A00Z&skoid=c275b492-fed2-47ef-8704-1bb10e68fcf2&sktid=72f988bf-86f1-41af-91ab-2d7cd011db47&skt=2024-11-27T03%3A02%3A36Z&ske=2024-12-03T03%3A02%3A00Z&sks=b&skv=2023-01-03&sr=c&sp=racwdxltf&sig=lNZS14iL13QSIUCLw1Aro%2FNqVZmNBnUg072a2L6AXT4%3D" "/our_data/code_data/data/output/test/qwen2_5-7b/v0-20241127-184146"
+./azcopy copy --recursive "https://xinzhanginterns.blob.core.windows.net/jiewu/data/sft_data?sv=2023-01-03&st=2025-01-28T07%3A19%3A58Z&se=2025-02-04T07%3A19%3A00Z&skoid=c275b492-fed2-47ef-8704-1bb10e68fcf2&sktid=72f988bf-86f1-41af-91ab-2d7cd011db47&skt=2025-01-28T07%3A19%3A58Z&ske=2025-02-04T07%3A19%3A00Z&sks=b&skv=2023-01-03&sr=c&sp=racwdxltf&sig=%2FfWj2c6V8wjpMDVK96AHvzYqnNfibfzwLmvMbnfAM4I%3D" "/our_data/code_data/data"
+# ./azcopy copy --recursive "https://xinzhanginterns.blob.core.windows.net/jiewu/data/output/test/qwen2_5-7b/v0-20241127-184146/checkpoint-7147?sv=2023-01-03&st=2024-11-27T03%3A02%3A36Z&se=2024-12-03T03%3A02%3A00Z&skoid=c275b492-fed2-47ef-8704-1bb10e68fcf2&sktid=72f988bf-86f1-41af-91ab-2d7cd011db47&skt=2024-11-27T03%3A02%3A36Z&ske=2024-12-03T03%3A02%3A00Z&sks=b&skv=2023-01-03&sr=c&sp=racwdxltf&sig=lNZS14iL13QSIUCLw1Aro%2FNqVZmNBnUg072a2L6AXT4%3D" "/our_data/code_data/data/output/test/qwen2_5-7b/v0-20241127-184146"
 
 # for split_id in $(seq 0 $((split_num-1))); do
 #     remainder=$((split_id % node_count))
